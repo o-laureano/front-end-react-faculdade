0450d07e599d8e42f09eb5ea51d0f9a1c0ef2be9
Henrique Laureano de Souza <henrique.souza@grupointegrado.br>
2024-05-29 20:42:13 -0300

    Inicializando repositório

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1 className="title">LAL de lal</h1>
+        <img src="https://i.ytimg.com/vi/e_fr3q4Fh20/maxresdefault.jpg" alt="lal de lal" />
+
+
+    </div>
+  );
+}
+
+export default App;
